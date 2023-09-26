58e5997fed6a50d529c09c8677a9950fd87dbc92
hjc <hjc_0827@outlook.com>
2023-09-26 23:00:43 +0800

    Add a manay to manay repository

---
+++ typeorm-relation-mapping3/src/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Article } from "./entity/Article";
+import { Tag } from "./entity/Tag";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "123456",
+  database: "typeorm_test",
+  synchronize: true,
+  logging: true,
+  entities: [Article, Tag, User],
+  migrations: [],
+  subscribers: [],
+  poolSize: 10,
+  connectorPackage: "mysql2",
+  extra: {
+    authPlugin: "sha256_password",
+  },
+});
