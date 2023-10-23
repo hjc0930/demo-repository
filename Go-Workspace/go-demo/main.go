d4db0b33d83e6f8e331d1723e40dd00e7d453ae9
Jiacheng Huang <hjc_0827@outlook.com>
2023-10-23 19:05:07 +0800

    Changes

---
+++ Go-Workspace/go-demo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello() {
+	fmt.Println("Hello Goroutine!")
+}
+func main() {
+	go hello()
+	fmt.Println("main goroutine done!")
+	time.Sleep(time.Second)
+}
