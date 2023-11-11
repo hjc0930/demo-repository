39fb7d3b902b67896d8675a5b299468f74bf3c50
hjc <hjc_0827@outlook.com>
2023-11-11 16:30:15 +0800

    a

--- react-context/src/router/index.tsx
+++ react-context/src/router/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReducerContext from "../views/reducer-context";
+import A from "../views/a";
 
 const router = createBrowserRouter([
   {
     path: "",
     element: <ReducerContext />,
+  },
+  {
+    path: "a",
+    element: <A />,
   },
 ]);
 
---
+++ react-context/src/views/a/index.tsx
@@ -0,0 +1,9 @@
+import Table from "../../../ant-design-master/components/table";
+
+function A() {
+  console.log(Table.EXPAND_COLUMN);
+
+  return <div>123</div>;
+}
+
+export default A;
