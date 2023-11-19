e1e58f2d502aa4ce22b6bfeec8fb0841b3f4509c
Jiacheng Huang <hjc_0827@outlook.com>
2023-11-19 22:33:04 +0800

    Replace an Array with a Set

--- react-context/src/utils/classesJoin.ts
+++ react-context/src/utils/classesJoin.ts
@@ -1,17 +1,17 @@
 function classesJoin(this: any, ...args: any[]): string {
   let i = 0;
-  const classes: string[] = [];
+  const classes = new Set<string>();
 
   while (i < args.length) {
     const arg = args[i++];
     if (!arg) continue;
 
     if (typeof arg === "string" || typeof arg === "number") {
-      classes.push((this && this[arg]) || arg);
+      classes.add((this && this[arg]) || arg);
     } else if (Array.isArray(arg) && arg.length) {
       const inner = classesJoin.apply(this, arg);
       if (inner) {
-        classes.push(inner);
+        classes.add(inner);
       }
     } else if (
       Object.prototype.toString.call(arg).toLocaleLowerCase() ===
@@ -21,19 +21,19 @@ function classesJoin(this: any, ...args: any[]): string {
         arg.toString !== Object.prototype.toString &&
         !arg.toString.toString().includes("[native code]")
       ) {
-        classes.push(arg.toString());
+        classes.add(arg.toString());
         continue;
       }
 
       for (const key in arg) {
         if (Reflect.has(arg, key) && arg[key]) {
-          classes.push((this && this[key]) || key);
+          classes.add((this && this[key]) || key);
         }
       }
     }
   }
 
-  return classes.join(" ");
+  return [...classes].join(" ");
 }
 
 export default classesJoin;
