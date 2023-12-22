95a2e25038144563b992fb76dc5a7a8ef5183a47
Jiacheng Huang <hjc_0827@outlook.com>
2023-12-22 23:07:14 +0800

    Update some code

--- webpack-config/index.js
+++ webpack-config/index.js
@@ -1,1 +1,17 @@
-console.log(process.env.npm_execpath.includes("pnpm"));
+var net = require("net");
+var server = net.createServer();
+
+server.once("error", function (err) {
+  if (err.code === "EADDRINUSE") {
+    // port is currently in use
+    console.log("port is currently in use");
+  }
+});
+
+server.once("listening", function () {
+  // close the server if listening doesn't fail
+  console.log("Port is currently available");
+  server.close();
+});
+
+server.listen(3000, "0.0.0.0");
