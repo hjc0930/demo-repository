68d3a74541b7e76fd120120081d76850057681f1
Jiacheng Huang <hjc_0827@outlook.com>
2024-01-22 02:37:12 +0800

    feat: Add a little demo codes

--- react-context/src/views/home/index.tsx
+++ react-context/src/views/home/index.tsx
@@ -1,26 +1,114 @@
-import { Button, Form, Input } from "antd";
+import { Button, Card, Form, Input, Space, Typography } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 
 function Home() {
   const [form] = Form.useForm();
 
-  const getData = () => {
-    console.log(form.getFieldValue("name"));
-  };
-
   return (
-    <>
-      <Button onClick={getData}>Get</Button>
-      <Form form={form}>
-        <Form.Item label="Name" name="name" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+    <Form
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 18 }}
+      form={form}
+      onFinish={(values) => {
+        console.log(values);
+      }}
+      style={{ maxWidth: 600 }}
+      autoComplete="off"
+    >
+      <Form.List name="items" initialValue={[{}]}>
+        {(fields, { add, remove }) => {
+          return (
+            <div
+              style={{ display: "flex", rowGap: 16, flexDirection: "column" }}
+            >
+              {fields.map((field) => {
+                return (
+                  <Card
+                    size="small"
+                    title={`Item ${field.name + 1}`}
+                    key={field.key}
+                    extra={
+                      <CloseOutlined
+                        onClick={() => {
+                          remove(field.name);
+                        }}
+                      />
+                    }
+                  >
+                    <Form.Item
+                      label="Name"
+                      name={[field.name, "name"]}
+                      rules={[{ required: true, message: "123123" }]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    {/* Nest Form.List */}
+                    <Form.Item label="List">
+                      <Form.List name={[field.name, "list"]}>
+                        {(subFields, subOpt) => (
+                          <div
+                            style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              rowGap: 16,
+                            }}
+                          >
+                            {subFields.map((subField) => (
+                              <Space key={subField.key}>
+                                <Form.Item
+                                  noStyle
+                                  name={[subField.name, "first"]}
+                                >
+                                  <Input placeholder="first" />
+                                </Form.Item>
+                                <Form.Item
+                                  noStyle
+                                  name={[subField.name, "second"]}
+                                >
+                                  <Input placeholder="second" />
+                                </Form.Item>
+                                <CloseOutlined
+                                  onClick={() => {
+                                    subOpt.remove(subField.name);
+                                  }}
+                                />
+                              </Space>
+                            ))}
+                            <Button
+                              type="dashed"
+                              onClick={() => subOpt.add()}
+                              block
+                            >
+                              + Add Sub Item
+                            </Button>
+                          </div>
+                        )}
+                      </Form.List>
+                    </Form.Item>
+                  </Card>
+                );
+              })}
+
+              <Button type="dashed" onClick={() => add()} block>
+                + Add Item
+              </Button>
+            </div>
+          );
+        }}
+      </Form.List>
+
+      <Form.Item noStyle shouldUpdate>
+        {() => (
+          <Typography>
+            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+          </Typography>
+        )}
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType="submit">Submit</Button>
+      </Form.Item>
+    </Form>
   );
 }
 
