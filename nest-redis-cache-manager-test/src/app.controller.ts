30dfcefdfed79cc5b6af673efbd743dad5a7256f
Jiacheng Huang <hjc_0827@outlook.com>
2024-02-10 12:28:11 +0800

    New Code

--- nest-redis-cache-manager-test/src/app.controller.ts
+++ nest-redis-cache-manager-test/src/app.controller.ts
@@ -1,11 +1,54 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Inject,
+  Query,
+  Res,
+  Session,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller()
 export class AppController {
   @Inject(CACHE_MANAGER)
   private cacheManager: Cache;
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  @Get('ttt')
+  ttt(
+    @Headers('authorization') authorization: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    if (authorization) {
+      try {
+        const token = authorization.split(' ')[1];
+        const data = this.jwtService.verify(token);
+
+        const newToken = this.jwtService.sign({
+          count: data.count + 1,
+        });
+        response.setHeader('token', newToken);
+        return data.count + 1;
+      } catch (e) {
+        console.log(e);
+        throw new UnauthorizedException();
+      }
+    } else {
+      const newToken = this.jwtService.sign({
+        count: 1,
+      });
+
+      response.setHeader('token', newToken);
+      return 1;
+    }
+  }
 
   @Get()
   getHello() {
@@ -28,5 +71,12 @@ export class AppController {
   async del() {
     await this.cacheManager.del('kkk');
     return 'done';
+  }
+
+  @Get('sss')
+  sss(@Session() session) {
+    console.log(session);
+    session.count = session.count ? session.count + 1 : 1;
+    return session.count;
   }
 }
--- nest-redis-cache-manager-test/src/app.module.ts
+++ nest-redis-cache-manager-test/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [
+    CacheModule.register(),
+    JwtModule.register({
+      secret: 'guang',
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
