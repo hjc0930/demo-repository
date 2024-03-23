0c9ba30bc33368b391c8babe1690d549ebc38cc7
Jiacheng Huang <hjc_0827@outlook.com>
2024-03-23 22:41:12 +0800

    New code

--- react-context/src/views/home/index.tsx
+++ react-context/src/views/home/index.tsx
@@ -1,7 +1,8 @@
 import { Badge } from "antd";
 
+const arr = [1, 2, 3, 4, 5, 6];
 function Home() {
-  return <Badge status="success" color="red" text="asdasdas" />;
+  return arr.map((item) => <p>{item}</p>);
 }
 
 export default Home;
