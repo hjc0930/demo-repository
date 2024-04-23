e3e59eef296ffb5ebcd45038d9fc57818727d2c1
Jiacheng Huang <hjc_0827@outlook.com>
2024-04-23 22:10:08 +0800

    refactor: Update some code

--- react-context/src/App.tsx
+++ react-context/src/App.tsx
@@ -1,7 +1,28 @@
-import Router from "./router";
+import { Button } from "antd";
+import useConfirm from "./hooks/useConfirm";
 
 function App() {
-  return <Router />;
+  const [confirm, context] = useConfirm();
+
+  const handleClick = () => {
+    confirm({
+      title: "Title",
+      content: "Content",
+      onOk: () => {
+        console.log("ok");
+      },
+      onCancel: () => {
+        console.log("cancel");
+      },
+    });
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick}>Add</Button>
+      {context}
+    </>
+  );
 }
 
 export default App;
---
+++ react-context/src/hooks/useConfirm.tsx
@@ -0,0 +1,41 @@
+import { Modal, ModalProps } from "antd";
+import React, { ReactNode, useState } from "react";
+
+interface ConfirmProps {
+  title?: ReactNode;
+  content?: ReactNode;
+  onOk?: () => void;
+  onCancel?: () => void;
+}
+
+const useConfirm = (): readonly [
+  (props: ConfirmProps) => void,
+  JSX.Element
+] => {
+  const [propsState, setPropsState] = useState<ModalProps>({});
+
+  const confirm = (props: ConfirmProps) => {
+    const { content, ...restProps } = props;
+    const onOk = () => {
+      props.onOk?.();
+      setPropsState({});
+    };
+    const onCancel = () => {
+      props?.onCancel?.();
+      setPropsState({});
+    };
+
+    setPropsState({
+      ...restProps,
+      open: true,
+      children: content,
+      onOk,
+      onCancel,
+    });
+  };
+  const context = <Modal {...propsState} destroyOnClose />;
+
+  return [confirm, context];
+};
+
+export default useConfirm;
