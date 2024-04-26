3e30676c5785ecd4829588aeb9dd1e4a7bb687f8
Jiacheng Huang <hjc_0827@outlook.com>
2024-04-26 19:53:08 +0800

    update

--- react-context/src/App.tsx
+++ react-context/src/App.tsx
@@ -1,40 +1,7 @@
-import { Button } from "antd";
-import { PropsWithChildren, createContext, useContext, useState } from "react";
-
-export const HeadContext = createContext<any>(0);
-
-const Child1 = () => {
-  const count = useContext(HeadContext);
-
-  console.log("Child1");
-  return <div>{count}</div>;
-};
-
-const Child2 = () => {
-  console.log("Child2");
-
-  return null;
-};
-const HeadProvider = (props: PropsWithChildren) => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <Button onClick={() => setCount((count) => count + 1)}>Count</Button>
-      <HeadContext.Provider value={count}>
-        {props.children}
-      </HeadContext.Provider>
-    </>
-  );
-};
+import Router from "./router";
 
 function App() {
-  return (
-    <HeadProvider>
-      <Child1 />
-      <Child2 />
-    </HeadProvider>
-  );
+  return <Router />;
 }
 
 export default App;
