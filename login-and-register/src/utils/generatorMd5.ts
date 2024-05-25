74fce511fd35c01f8f1a535c3cdb17f0c6c3df1e
Jiacheng Huang <hjc_0827@outlook.com>
2024-05-25 18:21:29 +0800

    Delete code

--- login-and-register/src/utils/generatorMd5.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-
-const generatorMd5 = (str: string) => {
-  const hash = crypto.createHash('md5');
-  hash.update(str);
-  return hash.digest('hex');
-};
-
-export default generatorMd5;
