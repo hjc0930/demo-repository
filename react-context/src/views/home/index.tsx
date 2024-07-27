6aa53b53cd1bb99fe9bb5367f83b5ac65ee99a62
Jiacheng Huang <hjc_0827@outlook.com>
2024-07-27 12:30:48 +0800

    updated code

--- react-context/src/views/home/index.tsx
+++ react-context/src/views/home/index.tsx
@@ -1,42 +1,21 @@
 import { Button } from "antd";
 import { notification } from "antd";
+import "./index.css";
 
 const Home = () => {
   const [api, contextHolder] = notification.useNotification();
 
   // const a: CSSMotionProps = {};
 
-  const handleOpen = () => {
-    api.open({
-      message: "DemoDemoDemo",
-      duration: 0,
-      placement: "top",
-    });
-
-    // setTimeout(() => {
-    //   console.log("timeout");
-
-    //   result();
-    // }, 3000);
-
-    // console.log(remove);
-
-    // msgApi.open({
-    //   content: "Hellow",
-    //   duration: 0,
-    // });
-    // messageApi
-    //   .open({
-    //     content: "Hellow",
-    //     duration: 0,
-    //   })
-    //   .then((r) => {
-    //     console.log(r);
-    //   });
-  };
+  const handleOpen = () => {};
 
   return (
     <>
+      <ul className="aaa-btn">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
       <Button onClick={handleOpen}>Open</Button>
       {/* {contextHolder} */}
       {contextHolder}
