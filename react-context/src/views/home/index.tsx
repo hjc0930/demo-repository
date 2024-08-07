9ca340c2c4d5036ce15377f218431e314045bb52
Jiacheng Huang <hjc_0827@outlook.com>
2024-08-07 23:04:46 +0800

    1123

--- react-context/src/views/home/index.tsx
+++ react-context/src/views/home/index.tsx
@@ -1,6 +1,11 @@
 import "./index.css";
 import Child2 from "./Child2";
 
-const Home = () => <Child2 />;
+const Home = () => (
+  <>
+    <Child1 />
+    <Child2 />
+  </>
+);
 
 export default Home;
