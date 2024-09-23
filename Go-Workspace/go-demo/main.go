452b3c2232885edc82173a0fe57d4b63271a2026
Jiacheng Huang <hjc_0827@outlook.com>
2024-09-23 22:55:07 +0800

    update: some code

--- Go-Workspace/go-demo/main.go
+++ Go-Workspace/go-demo/main.go
@@ -1,12 +1,19 @@
 package main
 
-type UserType struct {
-	id      int
-	name    string
-	age     int
-	address string
+import "fmt"
+
+func hanoi(n int, pillarStart string, pillarMid string, pillarTarget string) {
+	if n == 2 {
+		fmt.Printf("%s -> %s\n", pillarStart, pillarMid)
+		fmt.Printf("%s -> %s\n", pillarStart, pillarTarget)
+		fmt.Printf("%s -> %s\n", pillarMid, pillarTarget)
+		return
+	}
+	hanoi(n-1, pillarStart, pillarTarget, pillarMid)
+	fmt.Printf("%s -> %s\n", pillarStart, pillarTarget)
+	hanoi(n-1, pillarMid, pillarStart, pillarTarget)
 }
 
 func main() {
-
+	hanoi(4, "A", "B", "C")
 }
