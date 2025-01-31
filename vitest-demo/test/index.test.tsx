157fb4687bbcf0f277be7c7cf682e427908ff201
hjc <hjc_0827@outlook.com>
2025-01-31 10:15:16 +0800

    Remove the coverage file

--- vitest-demo/test/index.test.tsx
+++ vitest-demo/test/index.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import { routerConfig } from "../src/router/index"; // 假设 index.tsx 文件导出 Router
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { userEvent } from "@testing-library/user-event";
+import { useState } from "react";
 
 describe("Router", () => {
   test("renders home page", () => {
@@ -19,6 +21,26 @@ describe("Router", () => {
     render(<RouterProvider router={aboutMemoryRouter} />);
     await vi.waitFor(() => {
       expect(screen.getByText(/About/i)).not.toBeNull();
+    });
+  });
+
+  test("Change Demo", async () => {
+    const Demo = () => {
+      const [count, setCount] = useState(0);
+
+      return (
+        <div>
+          <p>{count}</p>
+          <button onClick={() => setCount(count + 1)}>Click</button>
+        </div>
+      );
+    };
+    render(<Demo />);
+
+    await userEvent.click(screen.getByText(/Click/i));
+
+    await vi.waitFor(() => {
+      expect(screen.getByText(/1/i)).not.toBeNull();
     });
   });
 });
