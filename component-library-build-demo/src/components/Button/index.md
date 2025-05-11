1d04c11fe1cd51960d6c94ebe786e094e22cb763
hjc <hjc_0827@outlook.com>
2025-05-11 22:27:13 +0800

    Add a react component library demo

---
+++ component-library-build-demo/src/components/Button/index.md
@@ -0,0 +1,9 @@
+# Foo
+
+This is an example component.
+
+```jsx
+import { Button } from 'component-demo';
+
+export default () => <Button>Button</Button>;
+```
---
+++ component-library-build-demo/src/components/Input/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const Input = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
+  return <input {...props} />;
+};
+
+export default Input;
