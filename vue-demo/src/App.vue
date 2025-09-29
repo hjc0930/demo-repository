5e89ec4ae3ac8599dee1c278db9b4f71e8c4b921
hjc <hjc_0827@outlook.com>
2025-09-29 22:04:59 +0800

    Add some demo codes

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ButtonComponent from './components/ButtonComponent.vue';
+
+const enum BusinessStatusEnum {
+  SUCCESS = "SUCCESS",
+  WARNING = "WARNING",
+  ERROR = "ERROR"
+}
+
+interface aaa {
+  b?: BusinessStatusEnum
+}
+
+const a: aaa = {
+  b: BusinessStatusEnum.WARNING
+}
+
+const is = a.b === BusinessStatusEnum.ERROR
+
+const count = ref(0);
+
+const onAdd = () => {
+  count.value++;
+}
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>{{ count }}</p>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <ButtonComponent @parent-count="onAdd">
+    <!-- <template #header>
+      header123123
+    </template> -->
+  </ButtonComponent>
+</template>
+
+<style scoped></style>
---
+++ vue-demo/src/components/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineEmits<{
+  parentCount: VoidFunction
+}>()
+</script>
+
+<template>
+  <h1 v-if="$slots.header">
+    <slot name="header" />
+  </h1>
+  <button @click="$emit('parentCount')">
+    <slot>Count</slot>
+  </button>
+
+</template>
