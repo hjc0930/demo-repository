1f41d64cc6f45f23d16efbe22756d2ce7f2a0f7e
hjc <hjc_0827@outlook.com>
2025-10-01 13:58:57 +0800

    Add some demo codes

---
+++ go-demo-project/go.mod
@@ -0,0 +1,3 @@
+module github.com/hjc0930/social
+
+go 1.25.1
--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import ButtonComponent from './components/ButtonComponent.vue';
-
-const enum BusinessStatusEnum {
-  SUCCESS = "SUCCESS",
-  WARNING = "WARNING",
-  ERROR = "ERROR"
-}
-
-interface aaa {
-  b?: BusinessStatusEnum
-}
-
-const a: aaa = {
-  b: BusinessStatusEnum.WARNING
-}
-
-const is = a.b === BusinessStatusEnum.ERROR
-
 const count = ref(0);
-
-const onAdd = () => {
-  count.value++;
-}
 
 </script>
 
@@ -33,11 +11,6 @@ const onAdd = () => {
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
-  <ButtonComponent @parent-count="onAdd">
-    <!-- <template #header>
-      header123123
-    </template> -->
-  </ButtonComponent>
 </template>
 
 <style scoped></style>
